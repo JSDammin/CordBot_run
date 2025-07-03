fcbe36d43403623c52de3c1d4cbc55a4b79760bd
JSDammin <justus2095@gmail.com>
2025-07-02 20:55:18 -0500

    Update deletecustom.js

--- Commands/Utility/deletecustom.js
+++ Commands/Utility/deletecustom.js
@@ -8,7 +8,7 @@ module.exports = {
     .addStringOption(opt =>
       opt.setName('name').setDescription('Name of the custom command').setRequired(true)
     ),
-  
+
   async execute(interaction) {
     if (!interaction.member.permissions.has('Administrator')) {
       return interaction.reply({ content: 'Only admins can use this command.', ephemeral: true });
@@ -18,9 +18,9 @@ module.exports = {
     const result = await CustomCommand.findOneAndDelete({ guildId: interaction.guildId, name });
 
     if (!result) {
-      return interaction.reply({ content: `Command \\`${name}\\` not found.`, ephemeral: true });
+      return interaction.reply({ content: `Command \`${name}\' not found.`, ephemeral: true });
     }
 
-    await interaction.reply({ content: `Custom command \\`${name}\\` deleted.`, ephemeral: true });
+    await interaction.reply({ content: `Custom command \`${name}\` deleted.`, ephemeral: true });
   }
 };
